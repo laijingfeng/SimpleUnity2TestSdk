6a48de205d3317ae80aedf1ba76542b869a4fa87
laijingfeng <337292616@qq.com>
2017-06-01 17:45:23 +0800

    add ant set tool

---
+++ AndroidProject/run_set_ant_with_lib.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+#coding=utf-8
+#version: 2017-06-01-00
+
+import os
+
+if __name__ == '__main__':
+    os.system('android update project --name UnityProject -p ./ --subprojects')
