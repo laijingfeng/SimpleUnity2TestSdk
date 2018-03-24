5dbbb30f65b0489a657005cb154f434dd706c543
laijingfeng <337292616@qq.com>
2018-03-24 18:32:05 +0800

    update

---
+++ UnityProject/Assets/Plugins/JerrySDK/iOS/JerrySDKBridge.h
@@ -0,0 +1,16 @@
+﻿#import <Foundation/Foundation.h>
+
+@interface JerrySDKBridge : NSObject
+
+@end
+
+#if defined (__cplusplus)
+extern "C"
+{
+#endif
+    
+    int AddC(int a, int b);
+    
+#if defined (__cplusplus)
+}
+#endif
---
+++ UnityProject/Assets/Plugins/JerrySDK/iOS/JerrySDKBridge.m
@@ -0,0 +1,22 @@
+﻿#import "JerrySDKBridge.h"
+
+@implementation JerrySDKBridge
+
++ (int)Add:(int)a and:(int)b{
+    return a + b;
+}
+
+@end
+
+#if defined (__cplusplus)
+extern "C"
+{
+#endif
+    
+    int AddC(int a, int b){
+        return [JerrySDKBridge Add:a and:b];
+    }
+    
+#if defined (__cplusplus)
+}
+#endif
